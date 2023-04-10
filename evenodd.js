9dcdd3536c7392dad2debb8381c7b89097dfeb30
kishanmasai260 <aktkishansingh@gmail.com>
2023-04-10 15:35:15 +0530

    evenodd

---
+++ evenodd.js
@@ -0,0 +1,9 @@
+//write a program to print even  or odd.
+
+let num=8;
+if(num%2==0){
+console.log("number is even");     //if number is devide by 2 and it gives reaminder 0 then it will be even number.
+}
+else{
+console.log("number is odd");  // if it gives remainder is not 0 then it will be odd nunber.
+}
